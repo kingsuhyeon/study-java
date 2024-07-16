4c03880c995b5c92c47608b2f32e00c08e1bdc62
kingsuhyeon <dvlproh@gmail.com>
2024-07-16 14:03:01 +0900

    섹션 7. 중첩 클래스, 내부 클래스1
    정적 중첩 클래스

---
+++ inflearn_java_mid1/src/nested/nested/NestedOuter.java
@@ -0,0 +1,19 @@
+package nested.nested;
+
+public class NestedOuter {
+    private static int outClassValue = 3;
+    private int outInstanceValue = 2;
+
+    static class Nested {
+        private int nestedInstanceValue = 1;
+
+        public void print(){
+            System.out.println(nestedInstanceValue);
+
+            // 바깥 클래스의 인스턴스 멤버에는 접근할 수 없다.
+            //System.out.println(outInstanceValue);
+
+            System.out.println(NestedOuter.outClassValue);
+        }
+    }
+}
