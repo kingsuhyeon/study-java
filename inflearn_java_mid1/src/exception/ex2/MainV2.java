5043e48648af96398cbea9274b88f7dd16fad437
kingsuhyeon <dvlproh@gmail.com>
2024-07-19 12:47:46 +0900

    섹션 10. 예외 처리2 - 실습
    예외 처리 도입3 - 정상, 예외 흐름 분리

--- inflearn_java_mid1/src/exception/ex2/MainV2.java
+++ inflearn_java_mid1/src/exception/ex2/MainV2.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 
 public class MainV2 {
     public static void main(String[] args) throws NetworkClientExceptionV2 {
-        NetworkServiceV2_2 networkService = new NetworkServiceV2_2();
+        NetworkServiceV2_3 networkService = new NetworkServiceV2_3();
 
         Scanner scanner = new Scanner(System.in);
         while (true) {
