442e6db390e037b787b7a9aea1573b2af6b612d3
kingsuhyeon <dvlproh@gmail.com>
2024-07-19 12:37:27 +0900

    섹션 10. 예외 처리2 - 실습
    예외 처리 도입2 - 예외 복구

--- inflearn_java_mid1/src/exception/ex2/MainV2.java
+++ inflearn_java_mid1/src/exception/ex2/MainV2.java
@@ -4,12 +4,13 @@ import java.util.Scanner;
 
 public class MainV2 {
     public static void main(String[] args) throws NetworkClientExceptionV2 {
-        NetworkServiceV2_1 networkService = new NetworkServiceV2_1();
+        NetworkServiceV2_2 networkService = new NetworkServiceV2_2();
 
         Scanner scanner = new Scanner(System.in);
         while (true) {
             System.out.print("전송할 문자: ");
             String input = scanner.nextLine();
+
             if (input.equals("exit")) {
                 break;
             }
